ef30ffe3af244e04b76d4974615e18d022854dc1
diegopenafiel <122368429+diegopenafiel@users.noreply.github.com>
2023-01-13 19:40:25 +0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,33 +1,106 @@
 
-import websockets
-import asyncio
-import base64
-import json
-import pyaudio
-import requests
-from streamlit_webrtc import WebRtcMode, webrtc_streamer
+import logging
+import logging.handlers
+import queue
+import threading
+import time
+import urllib.request
+from collections import deque
+from pathlib import Path
+from typing import List
 
+import numpy as np
+import pydub
 import streamlit as st
 
-if 'run' not in st.session_state:
-    st.session_state['run'] = False
+from streamlit_webrtc import WebRtcMode, webrtc_streamer
 
-Frames_per_buffer = 3200
-Format = pyaudio.paInt16
-Channels = 1
-Rate = 16000
-p = pyaudio.PyAudio()
+HERE = Path(__file__).parent
 
-# starts recording
-stream = p.open(
-    format=Format,
-    channels=Channels,
-    rate=Rate,
-    input=True,
-    frames_per_buffer=Frames_per_buffer
-)
+logger = logging.getLogger(__name__)
 
-webrtc_ctx = webrtc_streamer(
+
+# This code is based on https://github.com/streamlit/demo-self-driving/blob/230245391f2dda0cb464008195a470751c01770b/streamlit_app.py#L48  # noqa: E501
+def download_file(url, download_to: Path, expected_size=None):
+    # Don't download the file twice.
+    # (If possible, verify the download using the file length.)
+    if download_to.exists():
+        if expected_size:
+            if download_to.stat().st_size == expected_size:
+                return
+        else:
+            st.info(f"{url} is already downloaded.")
+            if not st.button("Download again?"):
+                return
+
+    download_to.parent.mkdir(parents=True, exist_ok=True)
+
+    # These are handles to two visual elements to animate.
+    weights_warning, progress_bar = None, None
+    try:
+        weights_warning = st.warning("Downloading %s..." % url)
+        progress_bar = st.progress(0)
+        with open(download_to, "wb") as output_file:
+            with urllib.request.urlopen(url) as response:
+                length = int(response.info()["Content-Length"])
+                counter = 0.0
+                MEGABYTES = 2.0 ** 20.0
+                while True:
+                    data = response.read(8192)
+                    if not data:
+                        break
+                    counter += len(data)
+                    output_file.write(data)
+
+                    # We perform animation by overwriting the elements.
+                    weights_warning.warning(
+                        "Downloading %s... (%6.2f/%6.2f MB)"
+                        % (url, counter / MEGABYTES, length / MEGABYTES)
+                    )
+                    progress_bar.progress(min(counter / length, 1.0))
+    # Finally, we remove these visual elements by calling .empty().
+    finally:
+        if weights_warning is not None:
+            weights_warning.empty()
+        if progress_bar is not None:
+            progress_bar.empty()
+
+
+def main():
+    st.header("Real Time Speech-to-Text")
+    st.markdown(
+        """
+This demo app is using [DeepSpeech](https://github.com/mozilla/DeepSpeech),
+an open speech-to-text engine.
+A pre-trained model released with
+[v0.9.3](https://github.com/mozilla/DeepSpeech/releases/tag/v0.9.3),
+trained on American English is being served.
+"""
+    )
+
+    # https://github.com/mozilla/DeepSpeech/releases/tag/v0.9.3
+    MODEL_URL = "https://github.com/mozilla/DeepSpeech/releases/download/v0.9.3/deepspeech-0.9.3-models.pbmm"  # noqa
+    LANG_MODEL_URL = "https://github.com/mozilla/DeepSpeech/releases/download/v0.9.3/deepspeech-0.9.3-models.scorer"  # noqa
+    MODEL_LOCAL_PATH = HERE / "models/deepspeech-0.9.3-models.pbmm"
+    LANG_MODEL_LOCAL_PATH = HERE / "models/deepspeech-0.9.3-models.scorer"
+
+    download_file(MODEL_URL, MODEL_LOCAL_PATH, expected_size=188915987)
+    download_file(LANG_MODEL_URL, LANG_MODEL_LOCAL_PATH, expected_size=953363776)
+
+    lm_alpha = 0.931289039105002
+    lm_beta = 1.1834137581510284
+    beam = 100
+
+    sound_only_page = "Sound only (sendonly)"
+    with_video_page = "With video (sendrecv)"
+
+    app_sst(
+        str(MODEL_LOCAL_PATH), str(LANG_MODEL_LOCAL_PATH), lm_alpha, lm_beta, beam
+    )
+
+
+def app_sst(model_path: str, lm_path: str, lm_alpha: float, lm_beta: float, beam: int):
+    webrtc_ctx = webrtc_streamer(
         key="speech-to-text",
         mode=WebRtcMode.SENDONLY,
         audio_receiver_size=1024,
@@ -35,82 +108,78 @@ webrtc_ctx = webrtc_streamer(
         media_stream_constraints={"video": False, "audio": True},
     )
 
-st.title('Create real_time transcription from your microphone')
+    status_indicator = st.empty()
 
-start, stop = st.columns(2)    
+    if not webrtc_ctx.state.playing:
+        return
 
-def stop_listening():
-    st.session_state['run'] = False
-    
+    status_indicator.write("Loading...")
+    text_output = st.empty()
+    stream = None
 
-def start_listening():
-    st.session_state['run'] = True
+    while True:
+        if webrtc_ctx.audio_receiver:
+            if stream is None:
+                from deepspeech import Model
 
+                model = Model(model_path)
+                model.enableExternalScorer(lm_path)
+                model.setScorerAlphaBeta(lm_alpha, lm_beta)
+                model.setBeamWidth(beam)
 
-start.button('Start Listening', on_click= start_listening)
+                stream = model.createStream()
 
+                status_indicator.write("Model loaded.")
 
-stop.button('Stop Listening', on_click= stop_listening)
+            sound_chunk = pydub.AudioSegment.empty()
+            try:
+                audio_frames = webrtc_ctx.audio_receiver.get_frames(timeout=1)
+            except queue.Empty:
+                time.sleep(0.1)
+                status_indicator.write("No frame arrived.")
+                continue
 
+            status_indicator.write("Running. Say something!")
 
-endpoint_url = "wss://api.assemblyai.com/v2/realtime/ws?sample_rate=16000"
+            for audio_frame in audio_frames:
+                sound = pydub.AudioSegment(
+                    data=audio_frame.to_ndarray().tobytes(),
+                    sample_width=audio_frame.format.bytes,
+                    frame_rate=audio_frame.sample_rate,
+                    channels=len(audio_frame.layout.channels),
+                )
+                sound_chunk += sound
 
-async def send_receive():
-
-    print(f'Connecting websocket to url ${endpoint_url}')
-
-    async with websockets.connect(
-        endpoint_url,
-        extra_headers=(("Authorization", st.secrets.key),),
-        ping_interval=5,
-        ping_timeout=20
-    ) as _ws:
-
-        r = await asyncio.sleep(0.1)
-        print("Receiving SessionBegins ...")
-
-        session_begins = await _ws.recv()
-        print(session_begins)
-        print("Sending messages ...")
-
-        async def send():
-            while st.session_state['run']:
-                try:
-                    audio_frames = webrtc_ctx.audio_receiver.get_frames(timeout=1)
-                    data = stream.read(Frames_per_buffer)
-                    data = base64.b64encode(data).decode("utf-8")
-                    json_data = json.dumps({"audio_data":str(data)})
-                    r = await _ws.send(json_data)
-
-                except websockets.exceptions.ConnectionClosedError as e:
-                    print(e)
-                    assert e.code == 4008
-                    break
-
-                except Exception as e:
-                    assert False, "Not a websocket 4008 error"
-
-                r = await asyncio.sleep(0.01)
-
-            return True
+            if len(sound_chunk) > 0:
+                sound_chunk = sound_chunk.set_channels(1).set_frame_rate(
+                    model.sampleRate()
+                )
+                buffer = np.array(sound_chunk.get_array_of_samples())
+                stream.feedAudioContent(buffer)
+                text = stream.intermediateDecode()
+                text_output.markdown(f"**Text:** {text}")
+        else:
+            status_indicator.write("AudioReciver is not set. Abort.")
+            break
 
 
-        async def receive():
-            while st.session_state['run']:
-                try:
-                    result_str = await _ws.recv()
-                    if json.loads(result_str)['message_type'] == 'FinalTranscript':
-                        print(json.loads(result_str)['text'])
-                        st.markdown(json.loads(result_str)['text'])
+if __name__ == "__main__":
+    import os
 
-                except websockets.exceptions.ConnectionClosedError as e:
-                    print(e)
-                    assert e.code == 4008
-                    break
+    DEBUG = os.environ.get("DEBUG", "false").lower() not in ["false", "no", "0"]
 
-                except Exception as e:
-                    assert False, "Not a websocket 4008 error"
+    logging.basicConfig(
+        format="[%(asctime)s] %(levelname)7s from %(name)s in %(pathname)s:%(lineno)d: "
+        "%(message)s",
+        force=True,
+    )
 
-        send_result, receive_result = await asyncio.gather(send(), receive())
+    logger.setLevel(level=logging.DEBUG if DEBUG else logging.INFO)
 
-asyncio.run(send_receive())     
+    st_webrtc_logger = logging.getLogger("streamlit_webrtc")
+    st_webrtc_logger.setLevel(logging.DEBUG)
+
+    fsevents_logger = logging.getLogger("fsevents")
+    fsevents_logger.setLevel(logging.WARNING)
+
+    main()
