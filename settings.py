f5240a2270412c1cc209f56a13c73c410e5e51fa
diegopenafiel <122368429+diegopenafiel@users.noreply.github.com>
2023-01-15 11:02:15 +0400

    Add files via upload

--- settings.py
+++ settings.py
@@ -21,6 +21,8 @@ METADATA_DIR_AUGMENTED_PROCESSED = os.path.join(METADATA_DIR_AUGMENTED, 'process
 LOG_DIR = os.path.join(ROOT_DIR, 'logs')
 LOG_DIR_TRAINING = os.path.join(LOG_DIR, 'training')
 
+MODEL_DIR = os.path.join(ROOT_DIR, 'models')
+MODEL_PATH = os.path.join(MODEL_DIR, 'model.pth')
 SENTIMENT_MODEL_URL = "https://azadpgsfnpqatuaensta.blob.core.windows.net/training/audio/whisper_class_model.pth?se=2023-01-12T08%3A03%3A22Z&ske=2023-01-19T07%3A03%3A22Z&skoid=5f490e73-06e5-4c43-a7c3-9f15899852f5&sks=b&skt=2023-01-12T07%3A03%3A22Z&sktid=3b618463-9352-4fa4-a67c-112da2837c29&skv=2021-04-10&sp=r&spr=https&sr=b&sv=2019-10-10&sig=tN25JCMMAeDHMICsOHVtxL%2FlSd9gIkCY5ep%2Bry3xIwU%3D"
 
 OUT_DIR = os.path.join(ROOT_DIR, 'output')
